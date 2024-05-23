3a77b9a5f728066bad25280987efcbd2eb4cd1b7
bostonwilliamss1 <bostonwilliamss1@gmail.com>
2024-05-23 12:12:14 -0600

    fixed some things

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -2,19 +2,19 @@ document.addEventListener('DOMContentLoaded', () => {
     let menuButton = document.querySelector('.menu-button');
     let menu = document.getElementById('menu');
 
-    console.log(menuButton);
-    console.log(menu);
+    if (menuButton && menu) {
+        menu.classList.add('hide');
 
-    menu.classList.add('hide');
+        function toggleMenu() {
+            menu.classList.toggle('hide');
+            console.log("Menu toggled");
+        }
 
-    function toggleMenu() {
-        menu.classList.toggle("hide");
-        console.log("Menu toggled");
+        menuButton.addEventListener("click", toggleMenu);
+    } else {
+        console.error("Menu button or menu not found.");
     }
 
-    menuButton.addEventListener("click", toggleMenu);
-
-    // Add event listener to gallery section
     let gallery = document.querySelector('.gallery');
     gallery.addEventListener('click', viewHandler);
 });
@@ -27,31 +27,23 @@ function viewerTemplate(pic, alt) {
 }
 
 function viewHandler(event) {
-    // Check if the clicked element is an image
     if (event.target.tagName === 'IMG') {
-        // Get the src attribute from the clicked image
         const srcAttribute = event.target.getAttribute('src');
 
-        // Split the src attribute value on the "-" character
         const srcParts = srcAttribute.split("-");
 
-        // Construct the new image file name by adding "-full.jpeg" to the first part of the array
         const newSrc = srcParts[0] + "-full.jpeg";
 
-        // Construct the viewer template HTML
         const viewerHTML = viewerTemplate(newSrc, "Enlarged Image");
 
-        // Insert the viewer template into the top of the body element
         document.body.insertAdjacentHTML("afterbegin", viewerHTML);
 
-        // Add a listener to the close button (X) that calls the closeViewer function when clicked
         const closeButton = document.querySelector('.close-viewer');
         closeButton.addEventListener('click', closeViewer);
     }
 }
 
 function closeViewer() {
-    // Remove the viewer from the DOM
     const viewer = document.querySelector('.viewer');
     viewer.remove();
 }
